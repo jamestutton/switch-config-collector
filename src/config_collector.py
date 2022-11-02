c2cf89b59acc6f2c4f91c44d145884fdcd3201f6
James Tutton <james.tutton@redcentricplc.com>
2022-11-02 13:56:52 +0000

    wip

--- src/config_collector.py
+++ src/config_collector.py
@@ -142,7 +142,7 @@ class Devices:
     def Pending(self):
         return self.device_collection.find(
             #filter={"locked_by": None, "locked_at": None, "attempts": {"$lt": self.max_attempts}},
-            filter={"Phase": "PHASE 2","attempts": {"$lt": self.max_attempts}}
+            filter={"Phase": "PHASE 2"}
             #sort=[("priority", pymongo.DESCENDING)],
         )
 
